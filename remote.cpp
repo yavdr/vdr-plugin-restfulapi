9defec448fcf1d49b81db07e43e74a794b8d49e9
Michael Eiler <eiler.mike@gmail.com>
2011-06-05 23:16:09 +0200

    small changes in remote feature

--- remote.cpp
+++ remote.cpp
@@ -2,8 +2,8 @@
 
 void RemoteResponder::reply(std::ostream& out, cxxtools::http::Request& request, cxxtools::http::Reply& reply)
 {
-  if (request.method() != "PUT") {
-     reply.httpReturn(403, "Only PUT method is support by the remote control");
+  if (request.method() != "POST") {
+     reply.httpReturn(403, "Only POST method is support by the remote control");
      return;
   }
 
@@ -22,62 +22,62 @@ void RemoteResponder::reply(std::ostream& out, cxxtools::http::Request& request,
 
 KeyPairList::KeyPairList()
 {
-  append( "kup" , 		kUp 		);
-  append( "kdown" , 		kDown 		);
-  append( "kmenu" , 		kMenu 		);
-  append( "kok" , 		kOk 		);
-  append( "kback" , 		kBack 		);
-  append( "kleft" , 		kLeft 		);
-  append( "kright" , 		kRight 		);
-  append( "kred" , 		kRed 		);
-  append( "kgreen" , 		kGreen 		);
-  append( "kyellow" , 		kYellow 	);
-  append( "kblue" , 		kBlue 		);
-  append( "k0" , 		k0 		);
-  append( "k1" , 		k1 		);
-  append( "k2" , 		k2 		);
-  append( "k3" , 		k3 		);
-  append( "k4" , 		k4 		);
-  append( "k5" , 		k5 		);
-  append( "k6" , 		k6 		);
-  append( "k7" , 		k7 		);
-  append( "k8" , 		k8 		);
-  append( "k9" , 		k9 		);
-  append( "kinfo" , 		kInfo 		);
-  append( "kplay" , 		kPlay 		);
-  append( "kpause" , 		kPause 		);
-  append( "kstop" , 		kStop 		);
-  append( "krecord" , 		kRecord 	);
-  append( "kfastfwd" , 		kFastFwd 	);
-  append( "kfastrew" , 		kFastRew 	);
-  append( "knext" , 		kNext 		);
-  append( "kprev" , 		kPrev 		);
-  append( "kpower" , 		kPower 		);
-  append( "kchanup" , 		kChanUp 	);
-  append( "kchandn" , 		kChanDn 	);
-  append( "kvolup" , 		kVolUp 		);
-  append( "kvoldn" , 		kVolDn 		);
-  append( "kmute" , 		kMute 		);
-  append( "kaudio" , 		kAudio 		);
-  append( "ksubtitles" , 	kSubtitles 	);
-  append( "kschedule" ,		kSchedule 	);
-  append( "kchannels" ,		kChannels 	);
-  append( "ktimers" , 		kTimers 	);
-  append( "krecordings" , 	kRecordings 	);
-  append( "ksetup" , 		kSetup 		);
-  append( "kcommands" ,		kCommands 	);
-  append( "kuser0" , 		kUser0 		);
-  append( "kuser1" , 		kUser1 		);
-  append( "kuser2" , 		kUser2 		);
-  append( "kuser3" , 		kUser3 		);
-  append( "kuser4" , 		kUser4 		);
-  append( "kuser5" , 		kUser5 		);
-  append( "kuser6" , 		kUser6 		);
+  append( "up" , 		kUp 		);
+  append( "down" , 		kDown 		);
+  append( "menu" , 		kMenu 		);
+  append( "ok" , 		kOk 		);
+  append( "back" , 		kBack 		);
+  append( "left" , 		kLeft 		);
+  append( "right" , 		kRight 		);
+  append( "red" , 		kRed 		);
+  append( "green" , 		kGreen 		);
+  append( "yellow" , 		kYellow 	);
+  append( "blue" , 		kBlue 		);
+  append( "0" , 		k0 		);
+  append( "1" , 		k1 		);
+  append( "2" , 		k2 		);
+  append( "3" , 		k3 		);
+  append( "4" , 		k4 		);
+  append( "5" , 		k5 		);
+  append( "6" , 		k6 		);
+  append( "7" , 		k7 		);
+  append( "8" , 		k8 		);
+  append( "9" , 		k9 		);
+  append( "info" , 		kInfo 		);
+  append( "play" , 		kPlay 		);
+  append( "pause" , 		kPause 		);
+  append( "stop" , 		kStop 		);
+  append( "record" , 		kRecord 	);
+  append( "fastfwd" , 		kFastFwd 	);
+  append( "fastrew" , 		kFastRew 	);
+  append( "next" , 		kNext 		);
+  append( "prev" , 		kPrev 		);
+  append( "power" , 		kPower 		);
+  append( "chanup" , 		kChanUp 	);
+  append( "chandn" , 		kChanDn 	);
+  append( "volup" , 		kVolUp 		);
+  append( "voldn" , 		kVolDn 		);
+  append( "mute" , 		kMute 		);
+  append( "audio" , 		kAudio 		);
+  append( "subtitles" , 	kSubtitles 	);
+  append( "schedule" ,		kSchedule 	);
+  append( "channels" ,		kChannels 	);
+  append( "timers" , 		kTimers 	);
+  append( "recordings" , 	kRecordings 	);
+  append( "setup" , 		kSetup 		);
+  append( "commands" ,		kCommands 	);
+  append( "user0" , 		kUser0 		);
+  append( "user1" , 		kUser1 		);
+  append( "user2" , 		kUser2 		);
+  append( "user3" , 		kUser3 		);
+  append( "user4" , 		kUser4 		);
+  append( "user5" , 		kUser5 		);
+  append( "user6" , 		kUser6 		);
   append( "kuser7" , 		kUser7 		);
-  append( "kuser8" , 		kUser8 		);
-  append( "kuser9" , 		kUser9 		);
-  append( "knone" , 		kNone 		);
-  append( "kkbd" , 		kKbd 		);
+  append( "user8" , 		kUser8 		);
+  append( "user9" , 		kUser9 		);
+  append( "none" , 		kNone 		);
+  append( "kbd" , 		kKbd 		);
 }
 
 KeyPairList::~KeyPairList()
