1c170febbee452d49cc1003847de8b288999a30e
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-06-06 05:59:55 +0200

    ChangeState: return real counters in XML output

--- changestate.cpp
+++ changestate.cpp
@@ -32,30 +32,28 @@ void ChangeStateResponder::reply(std::ostream& out, cxxtools::http::Request& req
 
 void ChangeStateResponder::replyJson(StreamExtension& se)
 {
-    
-std::ostringstream json;
-json
-    << "{"
-    << "\"statusVersion\":" << ChangeStateCounter::StatusVersion() << ","
-    << "\"channelsVersion\":" << ChangeStateCounter::ChannelsVersion() << ","
-    << "\"recordingsVersion\":" << ChangeStateCounter::RecordingsVersion() << ","
-    << "\"timersVersion\":" << ChangeStateCounter::TimersVersion() << ","
-    << "\"eventsVersion\":" << ChangeStateCounter::EventsVersion()
-    << "}";
+    std::ostringstream json;
+    json
+        << "{"
+        << "\"statusVersion\":" << ChangeStateCounter::StatusVersion() << ","
+        << "\"channelsVersion\":" << ChangeStateCounter::ChannelsVersion() << ","
+        << "\"recordingsVersion\":" << ChangeStateCounter::RecordingsVersion() << ","
+        << "\"timersVersion\":" << ChangeStateCounter::TimersVersion() << ","
+        << "\"eventsVersion\":" << ChangeStateCounter::EventsVersion()
+        << "}";
 
-std::string payload = json.str();
-se.write(payload.c_str());
-
+    std::string payload = json.str();
+    se.write(payload.c_str());
 }
 
 void ChangeStateResponder::replyXml(StreamExtension& se)
 {
     se.writeXmlHeader();
     se.write("<change-state>");
-    se.write("<statusVersion>0</statusVersion>");
-    se.write("<channelsVersion>0</channelsVersion>");
-    se.write("<recordingsVersion>0</recordingsVersion>");
-    se.write("<timersVersion>0</timersVersion>");
-    se.write("<eventsVersion>0</eventsVersion>");
+    se.write(cString::sprintf("<statusVersion>%llu</statusVersion>", (unsigned long long)ChangeStateCounter::StatusVersion()));
+    se.write(cString::sprintf("<channelsVersion>%llu</channelsVersion>", (unsigned long long)ChangeStateCounter::ChannelsVersion()));
+    se.write(cString::sprintf("<recordingsVersion>%llu</recordingsVersion>", (unsigned long long)ChangeStateCounter::RecordingsVersion()));
+    se.write(cString::sprintf("<timersVersion>%llu</timersVersion>", (unsigned long long)ChangeStateCounter::TimersVersion()));
+    se.write(cString::sprintf("<eventsVersion>%llu</eventsVersion>", (unsigned long long)ChangeStateCounter::EventsVersion()));
     se.write("</change-state>");
 }
