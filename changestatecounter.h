18ff6e9b9e91a83585c5ca6f64d0e3d8a9d11725
hotzenplotz5 <hotzenplotz5@gmx.de>
2026-06-07 11:33:10 +0200

    ChangeState: use atomic counters and restart marker

--- changestatecounter.h
+++ changestatecounter.h
@@ -1,11 +1,15 @@
 #ifndef __CHANGESTATECOUNTER_H
 #define __CHANGESTATECOUNTER_H
 
+#include <atomic>
 #include <stdint.h>
 
 class ChangeStateCounter
 {
 public:
+    static uint64_t InstanceStarted();
+    static uint64_t LastChange();
+
     static uint64_t StatusVersion();
     static uint64_t ChannelsVersion();
     static uint64_t RecordingsVersion();
@@ -17,6 +21,17 @@ public:
     static void IncrementRecordings();
     static void IncrementTimers();
     static void IncrementEvents();
+
+private:
+    static void MarkChanged(std::atomic<uint64_t>& counter);
+
+    static const uint64_t instanceStarted;
+    static std::atomic<uint64_t> lastChange;
+    static std::atomic<uint64_t> statusVersion;
+    static std::atomic<uint64_t> channelsVersion;
+    static std::atomic<uint64_t> recordingsVersion;
+    static std::atomic<uint64_t> timersVersion;
+    static std::atomic<uint64_t> eventsVersion;
 };
 
 #endif
